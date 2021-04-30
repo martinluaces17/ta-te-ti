a69488599311ebfb826e6fe87edf8df33965bc71
Martin Luaces <martinluaces17@gmail.com>
2021-04-30 19:30:23 +0000

    Tic-Tac-Toe

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -6,19 +6,25 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<h1>Ta-Te-Ti</h1>
+			<div className="container">
+				<div className="row">
+					<button className="col-4 col1"></button>
+					<button className="col-4 col2"></button>
+					<button className="col-4 col3"></button>
+				</div>
+				<div className="row">
+					<button className="col-4 col4"></button>
+					<button className="col-4 col5"></button>
+					<button className="col-4 col6"></button>
+				</div>
+				<div className="row">
+					<button className="col-4 col7"></button>
+					<button className="col-4 col8"></button>
+					<button className="col-4 col9"></button>
+				</div>
+			</div>
 		</div>
 	);
 }
